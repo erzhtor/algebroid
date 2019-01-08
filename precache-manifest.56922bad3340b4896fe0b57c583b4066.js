03e348d819ef69993b94367845a065ab826daeba
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2019-01-08 21:44:07 +0300

    Updates

---
+++ precache-manifest.56922bad3340b4896fe0b57c583b4066.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2749ab865fd6d71eea1",
+    "url": "/algebroid/static/js/runtime~main.a2749ab8.js"
+  },
+  {
+    "revision": "f1b22e4f2b4adee2fb3c",
+    "url": "/algebroid/static/js/main.f1b22e4f.chunk.js"
+  },
+  {
+    "revision": "3cd8b00fc26cb22dd818",
+    "url": "/algebroid/static/js/1.3cd8b00f.chunk.js"
+  },
+  {
+    "revision": "f1b22e4f2b4adee2fb3c",
+    "url": "/algebroid/static/css/main.50f6eec8.chunk.css"
+  },
+  {
+    "revision": "1c34fc603b2aacf6f5c538dd956182f1",
+    "url": "/algebroid/index.html"
+  }
+];
